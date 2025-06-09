57f6dedb1dc858219f9687311383deff6aa193bb
issaczs5 <issaczs005@gmail.com>
2025-06-09 15:38:17 -0700

    j

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta
 
 class estate_property_tag(models.Model):
     _name = "estate_property_offer"
@@ -8,3 +9,19 @@ class estate_property_tag(models.Model):
     status = fields.Selection([('accepted','Accepted'),('refused','Refused')],copy=False)
     partner_id = fields.Many2one("red.partner", required=True)
     property_id = fields.Many2one("estate_property", required=True)
+    vality = fields.Integer(default=7)
+    date_deadline = fields.Date()
+
+    @api.depends("vality")
+    def _deadline(self):
+        if self.vality and self.create_date:
+            self.date_deadline = self.create_date.date() + timedelta(days=self.vality)
+        else:
+            self.date_deadline = self.date_deadline
+
+    @api.depends("date_deadline")
+    def _vality(self):
+        if self.date_deadline and self.create_date:
+            self.vality = self.date_deadline() - timedelta(days=self.create_date)
+        else:
+            self.vality = self.vality
