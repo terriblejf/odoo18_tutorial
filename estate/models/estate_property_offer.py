9ff6a06558f71ac11d8f5461d51cadb4b394b99e
issaczs5 <issaczs005@gmail.com>
2025-06-16 17:02:58 -0700

    Update estate_property_offer.py

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -39,8 +39,6 @@ class estate_property_tag(models.Model):
                     return True
             if record.status == False:
                 record.status = "accepted"
-                record.property_id.salesperson = record.partner_id.id
-                record.property_id.selling_price = record.price
             return True
 
     def refuse_action(self):
