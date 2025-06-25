4d0e7fd88dc3356d406b4a20b09c72a1b6272e85
issaczs5 <issaczs005@gmail.com>
2025-06-25 16:33:17 -0700

    k

--- estate/models/inherit_res_users.py
+++ estate/models/inherit_res_users.py
@@ -1,6 +1,6 @@
 from odoo import fields, models
 
-class inherit_res_users(models.Model):
+class ResUsers(models.Model):
     _inherit = "res.users"
 
     property_ids = fields.One2many("estate_property", "salesperson", domain="[('state', 'in', ('new', 'offer_recived'))]")
